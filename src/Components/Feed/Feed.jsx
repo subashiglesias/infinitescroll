905086840797056d14ad614c4dce46c6aebca438
Subash P <subash.p@mrcooper.com>
2021-07-07 21:23:37 +0530

    Initial commit

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -3,16 +3,11 @@ import './Feed.css'
 import Card from "../Card/Card";
 
 const Feed = () => {
-    const [page, setPage] = useState(1)
-    const [state, updateState] = useState({
-        nextPage: 1,
-        posts: [],
-    })
+    const [feeds, updateFeeds] = useState({nextPage: 1, posts: []})
 
     const debounce = (fn, d) => {
         let timer;
-        return function () {
-            let args = arguments
+        return function (...args) {
             clearTimeout(timer);
             timer = setTimeout(() => {
                 fn.apply(this, args)
@@ -20,35 +15,38 @@ const Feed = () => {
         }
     }
 
-    function alert() {
-        alert("you're at the bottom of the page");
+    const alert = () => {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            fetch(`https://api.unsplash.com/photos/?page=${feeds.nextPage}&client_id=zMrt7EL6xz0a4VxaXGgdnBn6aveO2Q8E23O6bA6scSo`)
+                .then(response => response.json())
+                .then(data => {
+                    const {posts, nextPage} = feeds;
+                    console.log("Calling update feeds to set page ", {
+                        nextPage: (nextPage + 1),
+                        posts: [...posts, ...data]
+                    })
+                    updateFeeds({nextPage: (nextPage + 1), posts: [...posts, ...data]});
+                })
+        }
     }
 
+    let getAlert = debounce(alert, 1000);
+
     useEffect(() => {
+        alert();
         window.addEventListener('scroll', () => {
-
-            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-                debounce(alert, 10)
-            }
-
+            getAlert()
         })
-    }, [])
-
-    useEffect(() => {
-        fetch(`https://api.unsplash.com/photos/?page=${state.nextPage}&client_id=zMrt7EL6xz0a4VxaXGgdnBn6aveO2Q8E23O6bA6scSo`)
-            .then(response => response.json())
-            .then(data => {
-                const {posts, nextPage} = state;
-                updateState({nextPage: nextPage + 1, posts: [...posts, ...data]});
-
+        return () => {
+            window.removeEventListener('scroll', () => {
             })
+        }
     }, [])
-    console.log(state)
 
 
     return (<div className='app-feed'>
         {
-            state.posts.map((post) => <Card key={post.id} caption={post.alt_description} image={post.links.download}/>)
+            feeds.posts.map((post) => <Card key={post.id} caption={post.alt_description} image={post.links.download}/>)
         }
     </div>)
 }
